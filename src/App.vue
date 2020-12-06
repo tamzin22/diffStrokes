00198379215290cf1c4f1df4f71e8c1a2ff979de
tamzin22 <evelynetamzin@gmail.com>
2020-12-06 14:50:21 +0300

    first page

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app" class="container-fluid">
+  <div container p-3 my-3 border>
+  <home />
+    </div>
+   </div>
+</template>
+
+<script>
+import home from './components/home.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    home
+  }
+}
+</script>
+
+import default{
+
+}
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+ 
+}
+
+</style>
